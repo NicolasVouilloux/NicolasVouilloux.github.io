c4bae8b684f1e134a27fd183e115609d993bcea9
Nicolas Vouilloux <nicolasvcire@gmail.com>
2024-07-29 20:18:54 +0200

    Adding button for information and content in first page

--- JS/premierJs.js
+++ JS/premierJs.js
@@ -35,40 +35,28 @@ function changeToCompetence(){
                     </ul>
               </ol>`;
 }
-function changeToInformation(){
-    document.getElementById("MainId").innerHTML=`<ol>
-                <li>Soft skills</li>
+function changeToInfo(){
+    document.getElementById("MainId").innerHTML=`<h2>Nicolas Vouilloux Curriculum Vitae</h2>
+            <p>potentiel paragraphe de coverletter</p>
+            <p></p>
+            <ol>
+                <li>Etudes</li>
                   <ul>
-                    <li>Team Work</li>
-                    <li>Active Listerning</li>
-                    <li>Problem solving</li>
-                    <li>Agile Method</li>
-                    <li>Creativity</li>
-                    <li>Autonomy</li>
+                    <li>2023/2026 BUT Informatique Paarcours A</li>
+                    <li>2021 Baccalauréat technologique STI2D</li>
                   </ul>
-                <li>Hard Skills</li>
-                    <ul>
-                    <li>Front-End</li>
-                        <ul>
-                            <li>JavaScript</li>
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>WordPress</li>
-                        </ul>
-                    <li>Back-End</li>
-                        <ul>
-                            <li>Java</li>
-                            <li>C++</li>
-                            <li>Python</li>
-                            <li>SQL</li>
-                            <li>SysOp</li>
-                        </ul>
-                    <li>Design</li>
-                        <ul>
-                        <li>Logos</li>
-                        <li>Poster</li>
-                        <li>Canva</li>
-                        </ul>
-                    </ul>
+                <li>Experience professionnelles</li>
+                  <ul>
+                    <li>28/04/2023 - 28/08/2023 : CDD Kuzma Funéraire Porteur-Chauffeur</li>
+                    <li>19/01/2020 - 26/02/2020 : Stage Orchestra Conseiller vente, Gestion des Stocks</li>
+                    <li>28/03/2019 - 04/04/2029 : Stage de découverte Texhnocentre Renault</li>
+                  </ul>
+                <li>Projets</li>
+                  <ul>
+                    <li>Réalisation d'un site web personnel(JavaScript/ HTML/ CSS)</li>
+                    <li>Réalisation d'une base de données sur SQLite</li>
+                    <li>Réalisation d'une application complète de modification de données pour les JO2024 en Java avec BDD sur Oracle</li>
+                    <li>Réalisation de petit projet personnel Java C++ Python</li>
+                  </ul>
               </ol>`;
 }
