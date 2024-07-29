ffe7045e44af4d60e81a9747ba30322b28071a98
Nicolas Vouilloux <nicolasvcire@gmail.com>
2024-07-29 16:00:35 +0200

    First commit of the WEB/CV

---
+++ JS/premierJs.js
@@ -0,0 +1,74 @@
+function changeToCompetence(){
+    document.getElementById("MainId").innerHTML=`<ol>
+                <li>Soft skills</li>
+                  <ul>
+                    <li>Team Work</li>
+                    <li>Active Listerning</li>
+                    <li>Problem solving</li>
+                    <li>Agile Method</li>
+                    <li>Creativity</li>
+                    <li>Autonomy</li>
+                  </ul>
+                <li>Hard Skills</li>
+                    <ul>
+                    <li>Front-End</li>
+                        <ul>
+                            <li>JavaScript</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>WordPress</li>
+                        </ul>
+                    <li>Back-End</li>
+                        <ul>
+                            <li>Java</li>
+                            <li>C++</li>
+                            <li>Python</li>
+                            <li>SQL</li>
+                            <li>SysOp</li>
+                        </ul>
+                    <li>Design</li>
+                        <ul>
+                        <li>Logos</li>
+                        <li>Poster</li>
+                        <li>Canva</li>
+                        </ul>
+                    </ul>
+              </ol>`;
+}
+function changeToInformation(){
+    document.getElementById("MainId").innerHTML=`<ol>
+                <li>Soft skills</li>
+                  <ul>
+                    <li>Team Work</li>
+                    <li>Active Listerning</li>
+                    <li>Problem solving</li>
+                    <li>Agile Method</li>
+                    <li>Creativity</li>
+                    <li>Autonomy</li>
+                  </ul>
+                <li>Hard Skills</li>
+                    <ul>
+                    <li>Front-End</li>
+                        <ul>
+                            <li>JavaScript</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>WordPress</li>
+                        </ul>
+                    <li>Back-End</li>
+                        <ul>
+                            <li>Java</li>
+                            <li>C++</li>
+                            <li>Python</li>
+                            <li>SQL</li>
+                            <li>SysOp</li>
+                        </ul>
+                    <li>Design</li>
+                        <ul>
+                        <li>Logos</li>
+                        <li>Poster</li>
+                        <li>Canva</li>
+                        </ul>
+                    </ul>
+              </ol>`;
+}
